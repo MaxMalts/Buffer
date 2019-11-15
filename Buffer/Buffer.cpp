bdab8d0c081ca67ac2fc5e4fd0f552a4400a7b7e
MaxMalts <max-kurt@bk.ru>
2019-11-15 20:03:38 +0300

    Added Bscanf()

--- Buffer/Buffer.cpp
+++ Buffer/Buffer.cpp
@@ -7,7 +7,7 @@
 #define ERR_BUF {NULL, 0, -1, 0}
 
 /**
-*	Строка с курсором
+*	Буфер (строка с курсором)
 */
 
 struct buf_t {
@@ -19,6 +19,13 @@ struct buf_t {
 };
 
 
+
+/**
+*	Пересчитывает последний символ исходя из текущего положения курсора
+*
+*	@param buf Буфер
+*/
+
 void RecalcLastChar(buf_t* buf) {
 	assert(buf != NULL);
 
@@ -26,6 +33,18 @@ void RecalcLastChar(buf_t* buf) {
 		buf->lastChar = buf->cursor;
 	}
 }
+
+
+/**
+*	Создает буфер в режиме чтения
+*
+*	@param[in] mode Режим (должен быть 'r')
+*	@param str Строка, которую кладем в буфер
+*	@param[in] strSize Размер строки (буфера)
+*	@param[out] err Код ошибки: 1 - mode != 'r'; 0 - все прошло нормально
+*
+*	@return Созданный буфер (в случае ошибки равен ERR_BUF).
+*/
 
 buf_t BufConstructor(const char mode, char* str, const int strSize, int* err = NULL) {
 
@@ -45,9 +64,19 @@ buf_t BufConstructor(const char mode, char* str, const int strSize, int* err = N
 	buf.mode = 'r';
 	buf.lastChar = strSize - 1;
 	
-
+	*err = 0;
 	return buf;
 }
+
+
+/**
+*	Создает буфер в режиме записи
+*
+*	@param[in] mode Режим (должен быть 'w')
+*	@param[out] err Код ошибки: 1 - mode != 'w'; 0 - все прошло нормально
+*
+*	@return Созданный буфер (в случае ошибки равен ERR_BUF).
+*/
 
 buf_t BufConstructor(const char mode, int* err = NULL) {
 
@@ -72,6 +101,17 @@ buf_t BufConstructor(const char mode, int* err = NULL) {
 	return buf;
 }
 
+
+/**
+*	Пишет символ в буфер
+*
+*	@param buf Буфер
+*	@param[in] ch Символ
+*
+*	@return 1 - буфер не в режиме записи; 2 - не удалось увеличить размер буфера;\
+ 0 - все прошло нормально
+*/
+
 int Bputc(buf_t* buf, const char ch) {
 	assert(buf != NULL);
 
@@ -93,14 +133,42 @@ int Bputc(buf_t* buf, const char ch) {
 	return 0;
 }
 
+
+/**
+*	Читает символ из буфера
+*
+*	@param[in] buf Буфер
+*
+*	@return Считанный символ. При достижении конца буфера, возвращает EOB.\
+ Если вернулось -2, то буфер не в режиме чтения.
+*/
+
 char Bgetc(buf_t* buf) {
 	assert(buf != NULL);
+
+	if (buf->mode != 'r') {
+		return -2;
+	}
 
 	if (buf->cursor >= buf->size - 1) {
 		return EOB;
 	}
 	return buf->str[buf->cursor];
 }
+
+
+/**
+*	Аналогичная scanf функция для буфера
+*
+*	@param[in] buf Буфер
+*	@param[in] format Строка фармата
+*	@param[in] args Аргументы
+*
+*	@return В случае успеха возвращает количество записанныз символов.\
+ Если значение отрицательно, то произошли следующие ошибки:\
+ -403 - буфер не в режиме записи; -401 - не удалось увеличить размер буфера\
+ ост. - ошибка при записи, проверьте корректность входных параметров
+*/
 
 template<typename... args_t>
 int Bprintf(buf_t* buf, const char* format, args_t... args) {
@@ -132,16 +200,53 @@ int Bprintf(buf_t* buf, const char* format, args_t... args) {
 	return printLen;
 }
 
+
+/**
+*	Аналогичная scanf функция для буфера
+*
+*	@param[in] buf Буфер
+*	@param[in] format Строка фармата
+*	@param[in] args Аргументы
+*
+*	@return В случае успеха возвращает количество прочитанных параметров.\
+ Если значение отрицательно, то произошли следующие ошибки:\
+ -1 - буфер не в режиме чтения; -2 - ошибка при чтении,\
+ проверьте корректность входных параметров
+*/
+
+template<typename... args_t>
+int Bscanf(buf_t* buf, const char* format, args_t... args) {
+	assert(buf != NULL);
+	assert(format != NULL);
+
+	if (buf->mode != 'r') {
+		return -1;
+	}
+
+	int formatLen = strlen(format);
+	char* newFormat = (char*)calloc(formatLen + 3, sizeof(char));
+	sprintf(newFormat, "%s%%n", format);
+
+	int charsRead = 0;
+	int scanRet = sscanf(buf->str[buf->cursor], newFormat, args..., &charsRead);
+	if (scanRet != sizeof...(args)) {
+		return -2;
+	}
+
+	return scanRet;
+}
+
+
 /**
 *	Пишет строку в конец буфера
 *
 *	@param buf
 *	@param[in] str
 *
-*	@return 1 - буффер не в режиме записи; 2 - проблема при дописывании; 0 - все прошло нормально
+*	@return 1 - буфер не в режиме записи; 2 - проблема при дописывании; 0 - все прошло нормально
 */
 
-int bufcat(buf_t* buf, const char* str) {
+int Bufcat(buf_t* buf, const char* str) {
 	assert(buf != NULL);
 	assert(str != NULL);
 
@@ -182,29 +287,6 @@ int IsOneOfChars(const char ch, const char* chars) {
 	}
 	return 0;
 }
-
-///**
-//*	Считает размер буфера и записывает его
-//*
-//*	@param buf Буфер
-//*
-//*	@return Размер; -1 - буффер некорректный
-//*/
-//
-//int RecalcBufSize(buf_t* buf) {
-//	assert(buf != NULL);
-//
-//	if (buf->mode == 'r') {
-//		int res = strlen(buf->str);
-//		buf->size = res;
-//		return res;
-//	}
-//	if (buf->mode == 'w') {
-//		return buf->size;
-//	}
-//
-//	return -1;
-//}
 
 
 /**
